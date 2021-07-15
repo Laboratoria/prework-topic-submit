4c4ab349b0afeeb350e5449fd67b846b06492b2b
Danielalab <olabolacrayola@hotmail.com>
2021-07-15 13:35:11 -0500

    update mocks: prompts & sendProgressToApi

--- __mocks__/api-service.js
+++ __mocks__/api-service.js
@@ -1,7 +1,5 @@
 const getToken = jest.fn().mockImplementation(() => Promise.resolve('faketoken123456'));
-const sendProgressToApi = jest.fn()
-  .mockImplementationOnce(() => Promise.resolve('success message'))
-  .mockImplementationOnce(() => Promise.reject(new Error('error message when save progress.')));
+const sendProgressToApi = jest.fn(() => Promise.resolve('success message'));
 
 module.exports = {
   getToken,
