f02e89a7db417ff71332043513966e3ad4c60608
Danielalab <olabolacrayola@hotmail.com>
2021-07-15 13:34:11 -0500

    add submitProgress function

---
+++ submitProgress.js
@@ -0,0 +1,85 @@
+const prompts = require('prompts');
+const kleur = require('kleur');
+const path = require('path');
+
+// eslint-disable-next-line import/no-dynamic-require
+const { laboratoria } = require(path.join(process.cwd(), 'package.json'));
+const runTests = require('./runTests');
+const {
+  createLoader, validateEmail, validatePassword, showTestsResult,
+} = require('./utils');
+const { getToken, sendProgressToApi } = require('./api-service');
+
+const questions = [
+  {
+    type: 'text',
+    name: 'email',
+    message: 'Tu correo:',
+    validate: validateEmail,
+  },
+  {
+    type: 'password',
+    name: 'password',
+    message: 'Tu contraseña',
+    validate: validatePassword,
+  },
+];
+
+const buildProgress = (testsData) => {
+  const {
+    unitId,
+    partId,
+    exerciseId,
+    typeContent,
+  } = laboratoria;
+
+  const progressData = {
+    unitId,
+    partId,
+    exerciseId,
+    type: typeContent,
+    preworkType: 'redesign-prework-fe',
+    progress: {
+      testResults: {
+        ...testsData,
+      },
+      updatedAt: new Date(),
+      /* Add completedAt property only if tests passed
+          to save in BigQuery */
+      ...(testsData.state === 'PASS' && { completedAt: new Date() }),
+    },
+  };
+
+  return progressData;
+};
+
+function submitProgress() {
+  console.log(kleur.bold().italic('A continuación completa los siguientes datos:'));
+  return prompts(questions)
+    .then((response) => {
+      console.log('response', response);
+      if (response.email && response.password) {
+        let interval;
+        return getToken(response.email, response.password)
+          .then((token) => Promise.all([runTests(), token]))
+          .then(([testsData, token]) => {
+            console.log(kleur.bold(`Se terminó de correr los tests, estos son los resultados:\n\n${showTestsResult(testsData)}`));
+            const msg = kleur.bold().italic('Registrando progreso');
+            /* start spinner animation */
+            interval = createLoader(msg);
+            const progress = buildProgress(testsData);
+            return sendProgressToApi(progress, token);
+          })
+          .finally(() => {
+          /* stop spinner animation */
+            clearInterval(interval);
+          });
+      }
+      throw new Error('El correo y contraseña ingresados no son válidos');
+    });
+}
+
+module.exports = {
+  submitProgress,
+  buildProgress,
+};
--- tests/submitProgress.spec.js
+++ tests/submitProgress.spec.js
@@ -5,12 +5,49 @@ jest.mock('../api-service.js');
 
 const prompts = require('prompts');
 
-const { buildProgress, main } = require('..');
+const { buildProgress, submitProgress } = require('../submitProgress');
+const { sendProgressToApi } = require('../api-service');
 
 describe('buildProgress', () => {
-  it('should be return an object with progress data', () => {
+  it('should return an object with progress data & without completedAt property', () => {
     const fakeTestsData = {
       state: 'FAIL',
+      stats: {
+        suites: 1,
+        tests: 4,
+        passes: 1,
+        pending: 2,
+        failures: 1,
+        start: '2021-06-23T17:51:43.377Z',
+        end: '2021-06-23T17:51:46.711Z',
+        duration: 1.30,
+      },
+    };
+    const fakeProgressData = {
+      unitId: '03-prework',
+      partId: '06-js-basics',
+      exerciseId: '02-data-types-numbers',
+      type: 'practice',
+      preworkType: 'redesign-prework-fe',
+      progress: {
+        testResults: { ...fakeTestsData },
+        updatedAt: '2021-06-23T17:51:43.377Z',
+      },
+    };
+    const progressData = buildProgress(fakeTestsData);
+    const result = {
+      ...progressData,
+      progress: {
+        ...progressData.progress,
+        updatedAt: fakeProgressData.progress.updatedAt,
+      },
+    };
+    expect(result).toEqual(fakeProgressData);
+  });
+
+  it('should return an object with progress data & with completedAt property', () => {
+    const fakeTestsData = {
+      state: 'PASS',
       stats: {
         suites: 1,
         tests: 4,
@@ -47,34 +84,31 @@ describe('buildProgress', () => {
   });
 });
 
-describe('main', () => {
-  it('should be return an error message with wrong email & password', (done) => {
+describe('submitProgress', () => {
+  it('should return an error message with wrong email & password', (done) => {
     prompts
       .mockReturnValueOnce(Promise.resolve({ email: '', password: '' }));
 
-    main()
+    submitProgress()
       .catch((err) => {
         expect(err.message).toBe('El correo y contraseña ingresados no son válidos');
         done();
       });
   });
 
-  it('should be return a success message', (done) => {
-    prompts
-      .mockReturnValueOnce(Promise.resolve({ email: 'test@test', password: 'test123' }));
-
-    main()
+  it('should return a success message', (done) => {
+    submitProgress()
       .then((resp) => {
         expect(resp).toBe('success message');
         done();
       });
   });
 
-  it('should be return a error message when save progress', (done) => {
-    prompts
-      .mockReturnValueOnce(Promise.resolve({ email: 'test@test', password: 'test123' }));
+  it('should return a error message when save progress', (done) => {
+    sendProgressToApi
+      .mockImplementationOnce(() => Promise.reject(new Error('error message when save progress.')));
 
-    main()
+    submitProgress()
       .catch((err) => {
         expect(err.message).toBe('error message when save progress.');
         done();
